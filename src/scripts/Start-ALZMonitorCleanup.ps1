47360e3c269102f52372999c2a43228998183c7c
Matthew Bratschun <matthew.bratschun@microsoft.com>
2023-03-29 09:37:27 -0600

    wip: script, workflow test

--- src/scripts/Start-ALZMonitorCleanup.ps1
+++ src/scripts/Start-ALZMonitorCleanup.ps1
@@ -1,3 +1,4 @@
+#requires -module Az.ResourceGraph
 <#
 .SYNOPSIS
     This script cleans up the resources deployed by the ALZ-Monitor automation, including alerts, policy assignments, policy definitions, and policy assignment role assignments. 
@@ -15,3 +16,44 @@
 
 #>
 
+Function Start-ALZMonitorCleanup {
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
+    param()
+
+    $ErrorActionPreference = 'Stop'
+
+    # get all management groups -- used in graph query scope
+    $managementGroups = Get-AzManagementGroup
+
+    If ($managementGroups.count -eq 0) {
+        Write-Error "The command 'Get-AzManagementGroups' returned '0' groups. This script needs to run with Owner permissions on the Azure Landing Zones intermediate root management group to effectively clean up Policies and all related resources."
+    }
+
+    # get alert resources to delete
+    $alertResourceIds = Search-AzGraph -Query "Resources | where type in ('Microsoft.Insights/metricAlerts','Microsoft.Insights/activityLogAlerts') and tags['_deployed_by_alz_monitor'] ~= 'True' | project id" -ManagementGroup $managementGroups.Id | 
+        Select-Object -ExpandProperty Id
+    
+    If ($alertResourceIds.count -eq 100) {
+        Write-Error "Placeholder: pagination"
+    }
+
+    # get resource group to delete
+    $resourceGroupIds = Search-AzGraph -Query "ResourcesContainers | where type =~ 'Microsoft.Resources/ResourceGroups' and tags['_deployed_by_alz_monitor'] ~= 'True' | project id" -ManagementGroup $managementGroups.Id | 
+        Select-Object -ExpandProperty Id
+
+    # get policy assignments to delete
+    $policyAssignmentIds = Search-AzGraph -Query "policyresources | where type =~ 'microsoft.authorization/policyAssignments' | project name,metadata=parse_json(properties.metadata),type | where metadata._deployed_by_alz_monitor | project id" -ManagementGroup $managementGroups.Id | 
+    Select-Object -ExpandProperty Id
+
+    # get policy set definitions to delete
+    $policySetDefinitionIds = Search-AzGraph -Query "policyresources | where type =~ 'microsoft.authorization/policySetDefintions' | project name,metadata=parse_json(properties.metadata),type | where metadata._deployed_by_alz_monitor | project id" -ManagementGroup $managementGroups.Id | 
+    Select-Object -ExpandProperty Id
+
+    # get policy definitions to delete
+    $policyDefinitionIds = Search-AzGraph -Query "policyresources | where type =~ 'microsoft.authorization/policyDefintions' | project name,metadata=parse_json(properties.metadata),type | where metadata._deployed_by_alz_monitor | project id" -ManagementGroup $managementGroups.Id | 
+    Select-Object -ExpandProperty Id
+
+    # get policy assignment role assignments
+    
+
+}
