4b4e8bdce32322a7b7da465eed31ec0d8638ba6c
Bruno Gabrielli <bruno.gabrielli@microsoft.com>
2023-08-29 14:21:52 +0200

    Update Start-ALZMonitorCleanup.ps1
    Removed the force parameter to avoid error due to non existing parameter for the Remove-AzRoleAssignment cmdlet

--- src/scripts/Start-ALZMonitorCleanup.ps1
+++ src/scripts/Start-ALZMonitorCleanup.ps1
@@ -180,7 +180,7 @@ If (!$reportOnly.IsPresent) {
 
     # delete policy assignment role assignments
     Write-Host "Deleting role assignments..."
-    $roleAssignments | ForEach-Object { $_ | Remove-AzRoleAssignment -Force:$force -Confirm:(!$force) | Out-Null }
+    $roleAssignments | ForEach-Object { $_ | Remove-AzRoleAssignment -Confirm:(!$force) | Out-Null }
 
 }
 Else {
